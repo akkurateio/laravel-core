c10ec8cb23070e1b53b2cf3b4a176b4479dc852f
Camille <camille@subvitamine.com>
2021-01-14 14:08:05 +0100

    Update email inside tests

--- tests/Auth/RegisterApiTest.php
+++ tests/Auth/RegisterApiTest.php
@@ -17,33 +17,36 @@ class RegisterApiTest extends TestCase
         Config::set('laravel-auth.allow_register', true);
 
         $password = $this->faker->password(8, 20);
+        $email = $this->faker->safeEmail;
 
         $response = $this->post(route('api.auth.register', [
             'account_id' => $this->user->account->id,
             'firstname' => $this->faker->firstName,
             'lastname' => $this->faker->lastName,
-            'email' => 'email@test.fr',
+            'email' => $email,
             'password' => $password,
             'password_confirmation' => $password,
         ]));
         $response->assertStatus(201);
 
-        $user = User::where('email', 'email@test.fr')->first();
+        $user = User::where('email', $email)->first();
         $this->assertDatabaseHas('users', ['id' => $user->id]);
     }
 
     /** @test **/
     public function it_should_register_a_user_without_password()
     {
+        $email = $this->faker->safeEmail;
+
         $response = $this->post(route('api.auth.register', [
             'account_id' => $this->user->account->id,
             'firstname' => $this->faker->firstName,
             'lastname' => $this->faker->lastName,
-            'email' => 'email@test.fr',
+            'email' => $email,
         ]));
         $response->assertStatus(201);
 
-        $user = User::where('email', 'email@test.fr')->first();
+        $user = User::where('email', $email)->first();
         $this->assertDatabaseHas('users', ['id' => $user->id]);
     }
 }
--- tests/Auth/RegisterControllerTest.php
+++ tests/Auth/RegisterControllerTest.php
@@ -17,33 +17,36 @@ class RegisterControllerTest extends TestCase
         Config::set('laravel-auth.allow_register', true);
 
         $password = $this->faker->password(8, 20);
+        $email = $this->faker->safeEmail;
 
         $response = $this->post(route('register', [
             'account_id' => $this->user->account->id,
             'firstname' => $this->faker->firstName,
             'lastname' => $this->faker->lastName,
-            'email' => 'email@test.fr',
+            'email' => $email,
             'password' => $password,
             'password_confirmation' => $password,
         ]));
         $response->assertRedirect(config('laravel-core.admin.route'));
 
-        $user = User::where('email', 'email@test.fr')->first();
+        $user = User::where('email', $email)->first();
         $this->assertDatabaseHas('users', ['id' => $user->id]);
     }
 
     /** @test **/
     public function it_should_register_a_user_without_password()
     {
+        $email = $this->faker->safeEmail;
+
         $response = $this->post(route('register', [
             'account_id' => $this->user->account->id,
             'firstname' => $this->faker->firstName,
             'lastname' => $this->faker->lastName,
-            'email' => 'email@test.fr',
+            'email' => $email,
         ]));
         $response->assertRedirect(config('laravel-core.admin.route'));
 
-        $user = User::where('email', 'email@test.fr')->first();
+        $user = User::where('email', $email)->first();
         $this->assertDatabaseHas('users', ['id' => $user->id]);
     }
 
