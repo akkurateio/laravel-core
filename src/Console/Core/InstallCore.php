3154316586ec1a7258079b7c6484cef14df6bc8b
Camille <camille@subvitamine.com>
2021-01-19 14:36:37 +0100

    Add part of the install into the account-submodule package

--- src/Console/Core/InstallCore.php
+++ src/Console/Core/InstallCore.php
@@ -3,7 +3,6 @@
 namespace Akkurate\LaravelCore\Console\Core;
 
 use Illuminate\Console\Command;
-use Illuminate\Filesystem\Filesystem;
 
 class InstallCore extends Command
 {
@@ -28,19 +27,19 @@ class InstallCore extends Command
      */
     public function handle()
     {
-        // Publish Migrations...
-        $this->callSilent('vendor:publish', ['--tag' => 'core-migrations', '--force' => true]);
-
         //Change the api guard driver
         $this->replaceInFile("'driver' => 'token',", "'driver' => 'passport',", $this->laravel->configPath('auth.php'));
 
         // "Home" Route...
         $this->replaceInFile('/home', '/brain', app_path('Providers/RouteServiceProvider.php'));
 
-        // Run the install of the core package
-        $this->installCore();
+        // Copy files to the application and replace occurrences inside it.
+        $this->call('account-submodule:install');
 
-        $answer = $this->choice('You prefer to launch migrate fresh or simple migrate ?', ['simple', 'fresh'], 'simple');
+        $this->line('');
+        $this->line('One more things,');
+
+        $answer = $this->choice('You prefer to launch fresh migrate or simple migrate ?', ['simple', 'fresh'], 'simple');
 
         if ($answer === 'simple') {
             $this->call('migrate', ['--seed' => true]);
@@ -52,33 +51,6 @@ class InstallCore extends Command
         $this->call('passport:install');
         $this->line('');
         $this->call('back-components:install');
-    }
-
-    /**
-     * Copy the files
-     */
-    protected function installCore()
-    {
-        // Policies...
-        (new Filesystem)->copyDirectory(__DIR__.'/../../../stubs/app/Policies', app_path('Policies'));
-
-        // Factories...
-        copy(__DIR__.'/../../../database/factories/UserFactory.php', base_path('database/factories/UserFactory.php'));
-        copy(__DIR__.'/../../../database/factories/AccountFactory.php', base_path('database/factories/AccountFactory.php'));
-
-        // Service Providers...
-        copy(__DIR__.'/../../../stubs/app/Providers/AuthServiceProvider.php', app_path('Providers/AuthServiceProvider.php'));
-
-        // Seeders...
-        (new Filesystem)->copyDirectory(__DIR__.'/../../../database/seeders', base_path('database/seeders'));
-
-        // Models...
-        copy(__DIR__.'/../../../stubs/app/Models/User.php', app_path('Models/User.php'));
-        copy(__DIR__.'/../../../stubs/app/Models/Account.php', app_path('Models/Account.php'));
-
-        $this->line('');
-        $this->comment('Core package installed, just a last question before you go');
-        $this->line('');
     }
 
     /**
